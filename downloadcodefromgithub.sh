a8cdbe2966fb510cb5d84e249099b7165a3d1c4e
dharlabwustl <atulkumar@wustl.edu>
2022-12-22 14:41:41 -0600

    Commit day1

---
+++ downloadcodefromgithub.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+export MCR_CACHE_ROOT=/workinginput
+cd /software/
+rm -r /software/*
+git_link=${5}
+git clone ${git_link}
+y=${git_link%.git}
+git_dir=$(basename $y)
+#find /software/${git_dir}/* -type f -exec sed -i "s/python/\/root\/anaconda3\/envs\/tf\/bin\/python'/g" {} \;
+#find /software/${git_dir}/* -type f -exec sed -i "s/python/\/root\/anaconda3\/envs\/tf\/bin\/python'/g" {} \;
+mv ${git_dir}/* /software/
+cp -r  /Stroke_CT_Segmentation /software/
+chmod +x /software/Stroke_CT_Segmentation/*.sh
+chmod +x /software/*.sh
+for x in  /software/Stroke_CT_Segmentation/*/* ; do chmod +x $x ; done
+for x in  /software/Stroke_CT_Segmentation/* ; do chmod +x $x ; done
+find /software/Stroke_CT_Segmentation/* -type f -exec sed -i "s/\/Stroke_CT_Segmentation/\/software\/Stroke_CT_Segmentation/g" {} \;
+#
+#cd /software/
+#rm -r /software/*
+#git_link=${5}
+#git clone ${git_link}
+#y=${git_link%.git}
+#git_dir=$(basename $y)
+#mv ${git_dir}/* /software/
+#cp -r  /Stroke_CT_Processing/* /software/
+#chmod +x /software/*.sh
+
+SESSION_ID=${1}
+XNAT_USER=${2}
+XNAT_PASS=${3}
+XNAT_HOST=${4}
+TYPE_OF_PROGRAM=${6}
+
+/software/script_to_call_main_program.sh $SESSION_ID $XNAT_USER $XNAT_PASS ${XNAT_HOST} ${TYPE_OF_PROGRAM}
---
+++ script_to_call_main_program.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SESSION_ID=${1}
+XNAT_USER=${2}
+XNAT_PASS=${3}
+XNAT_HOST=${4}
+TYPE_OF_PROGRAM=${5}
+echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM
+if [[ ${TYPE_OF_PROGRAM} == 2 ]] ;
+then
+    /software/ct_segmentation.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
+fi
+#if [[ ${TYPE_OF_PROGRAM} == 1 ]] ;
+#then
+#    /software/dicom2nifti_call_sessionlevel_selected.sh  ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
+#fi

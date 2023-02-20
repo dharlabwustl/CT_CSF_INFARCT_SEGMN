b94321e4ad6ca4ef9354678278626634d9258e26
dharlabwustl <atulkumar@wustl.edu>
2023-02-20 11:49:42 -0600

    Commit day10

--- ct_ich_segmentation.sh
+++ ct_ich_segmentation.sh
@@ -167,51 +167,51 @@ output_dirname=${working_dir}
 echo working_dir::${working_dir}
 echo output_dirname::${output_dirname}
 copy_allfiles_data   ${sessionID}  ${scanID} ${resource_dirname} ${output_dirname}
-#####################
-##/bin/bash -i -c
-##/root/anaconda3/bin/conda activate tf
-#/software/unet_ich_edema/ppredict.sh ${output_dirname}
-#
-#######################################################################################################################
-###/root/anaconda3/bin/conda deactivate
-##if [[ "$STR" == *"$SUB"* ]]; then
-##  echo "It's there."
-##fi
-#
-#for file in /software/unet_ich_edema/Results_model_114/*.nii*
-#do
-#  if [[ "${file}" != *"ERICH"* ]]; then
-#    echo "It's there."
-#      new_filename=${file%.nii.gz_class1*}_class1.nii.gz
-#        mv ${file} ${new_filename}
-#      cp ${new_filename} ${final_output_directory}/
-#  fi
-#
-#
-#done
-#
-#for file in /software/unet_ich_edema/Results_model_72/*.nii*
-#do
-#    if [[ "${file}" != *"ERICH"* ]]; then
-#      echo "It's there."
-#  new_filename=${file%.nii.gz_class2*}_class2.nii.gz
-#  mv ${file} ${new_filename}
-#  cp ${new_filename} ${final_output_directory}/
-#  fi
-#done
-#######################################################################################################################
-#
-#######################################################################################################################
-## COPY IT TO THE SNIPR RESPECTIVE SCAN RESOURCES
-#
-#snipr_output_foldername="MASKS"
-#file_suffixes=(  .nii.gz .nii .txt ) #sys.argv[5]
-#for file_suffix in ${file_suffixes[@]}
-#do
-#    echo "COPYING FILES TO ${snipr_output_foldername} "
-#    copyoutput_to_snipr  ${sessionID} ${scanID} "${final_output_directory}"  ${snipr_output_foldername}  ${file_suffix}
-#done
-#######################################################################################################################
+####################
+#/bin/bash -i -c
+#/root/anaconda3/bin/conda activate tf
+/software/unet_ich_edema/ppredict.sh ${output_dirname}
+
+######################################################################################################################
+##/root/anaconda3/bin/conda deactivate
+#if [[ "$STR" == *"$SUB"* ]]; then
+#  echo "It's there."
+#fi
+
+for file in /software/unet_ich_edema/Results_model_114/*.nii*
+do
+  if [[ "${file}" != *"ERICH"* ]]; then
+    echo "It's there."
+      new_filename=${file%.nii.gz_class1*}_class1.nii.gz
+        mv ${file} ${new_filename}
+      cp ${new_filename} ${final_output_directory}/
+  fi
+
+
+done
+
+for file in /software/unet_ich_edema/Results_model_72/*.nii*
+do
+    if [[ "${file}" != *"ERICH"* ]]; then
+      echo "It's there."
+  new_filename=${file%.nii.gz_class2*}_class2.nii.gz
+  mv ${file} ${new_filename}
+  cp ${new_filename} ${final_output_directory}/
+  fi
+done
+######################################################################################################################
+
+######################################################################################################################
+# COPY IT TO THE SNIPR RESPECTIVE SCAN RESOURCES
+
+snipr_output_foldername="MASKS"
+file_suffixes=(  .nii.gz .nii .txt ) #sys.argv[5]
+for file_suffix in ${file_suffixes[@]}
+do
+    echo "COPYING FILES TO ${snipr_output_foldername} "
+    copyoutput_to_snipr  ${sessionID} ${scanID} "${final_output_directory}"  ${snipr_output_foldername}  ${file_suffix}
+done
+######################################################################################################################
 
 
 

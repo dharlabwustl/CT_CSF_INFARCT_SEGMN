dc8d5c951e10078cb4278395cfcace6a3ca37d70
dharlabwustl <atulkumar@wustl.edu>
2023-05-15 14:17:35 -0500

    Commit day10

--- ct_ich_segmentation.sh
+++ ct_ich_segmentation.sh
@@ -145,6 +145,27 @@ get_maskfile_scan_metadata()" ${sessionId}  ${scanId}  ${resource_foldername} ${
 ## GET THE SINGLE CT NIFTI FILE NAME AND COPY IT TO THE WORKING_DIR
 niftifile_csvfilename=${working_dir}/'this_session_final_ct.csv'
 get_nifti_scan_uri ${sessionID}  ${working_dir} ${niftifile_csvfilename}
+########################################
+outputfiles_present=0
+while IFS=',' read -ra array; do
+scanID=${array[2]}
+echo sessionId::${sessionID}
+echo scanId::${scanID}
+resource_foldername="PREPROCESS_SEGM"
+### check if the file exists:
+call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID}  ${scanID}  ${resource_foldername} _resaved.nii.gz  _resaved_4DL_normalized.nii.gz   _resaved_levelset.nii.gz  _resaved_4DL_seg.nii.gz  _resaved_levelset_bet.nii.gz   manual_splits.txt ) ## _resaved_4DL_normalized.nii.gz_csf_3.nii.gz  _resaved_4DL_normalized.nii.gz_infarct.nii.gz  _resaved_4DL_normalized.nii.gz_csf_4.nii.gz  _resaved_4DL_normalized.nii.gz_csf_8.nii.gz  _resaved_4DL_normalized.nii.gz_csf_1.nii.gz   _resaved_4DL_normalized.nii.gz_csf_6.nii.gz   _resaved_4DL_normalized.nii.gz_csf_2.nii.gz   _resaved_4DL_normalized.nii.gz_csf_5.nii.gz  _resaved_4DL_normalized.nii.gz_csf_7.nii.gz  _resaved_4DL_normalized.nii.gz_csf_9.nii.gz  _resaved_4DL_normalized.nii.gz_csf_10.nii.gz)
+outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}" )
+done < <( tail -n +2 "${niftifile_csvfilename}" )
+################################################
+echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
+#echo "outputfiles_present::ATUL${outputfiles_present}::outputfiles_present"
+if [[  "${outputfiles_present: -1}" -eq 1 ]] ; then
+  echo " I AM THE ONE"
+fi
+if [[  "${outputfiles_present: -1}" -eq 0 ]] ; then
+
+echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
+
 copy_scan_data ${niftifile_csvfilename} ${working_dir}
 working_dir=/workinginput
 output_directory=/workingoutput
@@ -219,6 +240,12 @@ do
 done
 ######################################################################################################################
 
+######################################################################################################################
+echo " FILES NOT PRESENT I AM WORKING ON IT"
+else
+  echo " FILES ARE PRESENT "
+######################################################################################################################
+fi
 
 
 ################################################################################################################

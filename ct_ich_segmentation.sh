e01cadc558d5d87577a51ebe0819d6a33722bd36
dharlabwustl <atulkumar@wustl.edu>
2023-06-02 16:32:27 -0500

    Commit day10

--- ct_ich_segmentation.sh
+++ ct_ich_segmentation.sh
@@ -8,54 +8,53 @@ output_directory=/workingoutput
 
 final_output_directory=/outputinsidedocker
 
-copyoutput_to_snipr(){
-sessionID=$1
-scanID=$2
-resource_dirname=$4 #"MASKS" #sys.argv[4]
-file_suffix=$5
-output_dir=$3
-echo " I AM IN copyoutput_to_snipr "
-/opt/conda/bin/python -c "
+copyoutput_to_snipr() {
+  sessionID=$1
+  scanID=$2
+  resource_dirname=$4 #"MASKS" #sys.argv[4]
+  file_suffix=$5
+  output_dir=$3
+  echo " I AM IN copyoutput_to_snipr "
+  /opt/conda/bin/python -c "
 import sys
 sys.path.append('/software');
 from download_with_session_ID import *;
-uploadfile()" ${sessionID} ${scanID} ${output_dir} ${resource_dirname} ${file_suffix}  # ${infarctfile_present}  ##$static_template_image $new_image $backslicenumber #$single_slice_filename
+uploadfile()" ${sessionID} ${scanID} ${output_dir} ${resource_dirname} ${file_suffix} # ${infarctfile_present}  ##$static_template_image $new_image $backslicenumber #$single_slice_filename
 
 }
-
 
 copy_masks_data() {
-echo " I AM IN copy_masks_data "
-# rm -r /ZIPFILEDIR/*
-sessionID=${1}
-scanID=${2}
-resource_dirname=${3} #str(sys.argv[4])
-output_dirname=${4}  #str(sys.argv[3])
-echo output_dirname::${output_dirname}
-/opt/conda/bin/python -c "
+  echo " I AM IN copy_masks_data "
+  # rm -r /ZIPFILEDIR/*
+  sessionID=${1}
+  scanID=${2}
+  resource_dirname=${3} #str(sys.argv[4])
+  output_dirname=${4}   #str(sys.argv[3])
+  echo output_dirname::${output_dirname}
+  /opt/conda/bin/python -c "
 import sys
 sys.path.append('/software');
 from download_with_session_ID import *;
-downloadfiletolocaldir()" ${sessionID}  ${scanID}  ${resource_dirname}  ${output_dirname}    ### ${infarctfile_present}  ##$static_template_image $new_image $backslicenumber #$single_slice_filename
-
+downloadfiletolocaldir()" ${sessionID} ${scanID} ${resource_dirname} ${output_dirname} ### ${infarctfile_present}  ##$static_template_image $new_image $backslicenumber #$single_slice_filename
 
 }
+
 copy_allfiles_data() {
-echo " I AM IN copy_masks_data "
-# rm -r /ZIPFILEDIR/*
-sessionID=${1}
-scanID=${2}
-resource_dirname=${3} #str(sys.argv[4])
-output_dirname=${4}  #str(sys.argv[3])
-echo output_dirname::${output_dirname}
-/opt/conda/bin/python -c "
+  echo " I AM IN copy_masks_data "
+  # rm -r /ZIPFILEDIR/*
+  sessionID=${1}
+  scanID=${2}
+  resource_dirname=${3} #str(sys.argv[4])
+  output_dirname=${4}   #str(sys.argv[3])
+  echo output_dirname::${output_dirname}
+  /opt/conda/bin/python -c "
 import sys
 sys.path.append('/software');
 from download_with_session_ID import *;
-downloadallfiletolocaldir()" ${sessionID}  ${scanID}  ${resource_dirname}  ${output_dirname}    ### ${infarctfile_present}  ##$static_template_image $new_image $backslicenumber #$single_slice_filename
-
+downloadallfiletolocaldir()" ${sessionID} ${scanID} ${resource_dirname} ${output_dirname} ### ${infarctfile_present}  ##$static_template_image $new_image $backslicenumber #$single_slice_filename
 
 }
+
 #copy_scan_data() {
 #		echo " I AM IN copy_scan_data "
 ## rm -r /ZIPFILEDIR/*
@@ -76,69 +75,57 @@ downloadallfiletolocaldir()" ${sessionID}  ${scanID}  ${resource_dirname}  ${out
 #
 #}
 
-
-
-
-
-
-
 # #####################################################
-get_nifti_scan_uri(){
-# csvfilename=sys.argv[1]
-# dir_to_save=sys.argv[2]
-# echo " I AM IN copy_scan_data "
-# rm -r /ZIPFILEDIR/*
+get_nifti_scan_uri() {
+  # csvfilename=sys.argv[1]
+  # dir_to_save=sys.argv[2]
+  # echo " I AM IN copy_scan_data "
+  # rm -r /ZIPFILEDIR/*
 
-sessionID=$1
-working_dir=${2}
-output_csvfile=${3}
-rm -r ${working_dir}/*
-output_dir=$(dirname ${output_csvfile})
-rm -r ${output_dir}/*
-# scanID=$2
-/opt/conda/bin/python -c "
+  sessionID=$1
+  working_dir=${2}
+  output_csvfile=${3}
+  rm -r ${working_dir}/*
+  output_dir=$(dirname ${output_csvfile})
+  rm -r ${output_dir}/*
+  # scanID=$2
+  /opt/conda/bin/python -c "
 import sys
 sys.path.append('/software');
 from download_with_session_ID import *;
-call_decision_which_nifti()" ${sessionID}  ${working_dir} ${output_csvfile}
+call_decision_which_nifti()" ${sessionID} ${working_dir} ${output_csvfile}
 
 }
-
-
-
 
 copy_scan_data() {
-csvfilename=${1} #sys.argv[1]
-dir_to_save=${2} #sys.argv[2]
-# 		echo " I AM IN copy_scan_data "
-# rm -r /ZIPFILEDIR/*
-# rm -r ${working_dir}/*
-# rm -r ${output_dir}/*
-# sessionID=$1
-# # scanID=$2
-/opt/conda/bin/python -c "
+  csvfilename=${1} #sys.argv[1]
+  dir_to_save=${2} #sys.argv[2]
+  # 		echo " I AM IN copy_scan_data "
+  # rm -r /ZIPFILEDIR/*
+  # rm -r ${working_dir}/*
+  # rm -r ${output_dir}/*
+  # sessionID=$1
+  # # scanID=$2
+  /opt/conda/bin/python -c "
 import sys
 sys.path.append('/software');
 from download_with_session_ID import *;
-downloadniftiwithuri_withcsv()" ${csvfilename}  ${dir_to_save}
-
+downloadniftiwithuri_withcsv()" ${csvfilename} ${dir_to_save}
 
 }
 
-
-getmaskfilesscanmetadata()
-{
-# def get_maskfile_scan_metadata():
-sessionId=${1} #sys.argv[1]
-scanId=${2} # sys.argv[2]
-resource_foldername=${3} # sys.argv[3]
-dir_to_save=${4} # sys.argv[4]
-csvfilename=${5} # sys.argv[5]
-/opt/conda/bin/python -c "
+getmaskfilesscanmetadata() {
+  # def get_maskfile_scan_metadata():
+  sessionId=${1}           #sys.argv[1]
+  scanId=${2}              # sys.argv[2]
+  resource_foldername=${3} # sys.argv[3]
+  dir_to_save=${4}         # sys.argv[4]
+  csvfilename=${5}         # sys.argv[5]
+  /opt/conda/bin/python -c "
 import sys
 sys.path.append('/software');
 from download_with_session_ID import *;
-get_maskfile_scan_metadata()" ${sessionId}  ${scanId}  ${resource_foldername} ${dir_to_save} ${csvfilename}
+get_maskfile_scan_metadata()" ${sessionId} ${scanId} ${resource_foldername} ${dir_to_save} ${csvfilename}
 }
 
 #########################################################################
@@ -150,109 +137,105 @@ outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files
 echo '$outputfiles_present'::$outputfiles_present
 ########################################
 for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
-#  outputfiles_present=0
+  #  outputfiles_present=0
   echo $niftifile_csvfilename
-########################################
-outputfiles_present=0
-while IFS=',' read -ra array; do
-scanID=${array[2]}
-echo sessionId::${sessionID}
-echo scanId::${scanID}
-resource_foldername="PREPROCESS_SEGM"
-### check if the file exists:
-call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID}  ${scanID}  ${resource_foldername} _resaved.nii.gz  _resaved_4DL_normalized.nii.gz   _resaved_levelset.nii.gz  _resaved_4DL_seg.nii.gz  _resaved_levelset_bet.nii.gz   manual_splits.txt ) ## _resaved_4DL_normalized.nii.gz_csf_3.nii.gz  _resaved_4DL_normalized.nii.gz_infarct.nii.gz  _resaved_4DL_normalized.nii.gz_csf_4.nii.gz  _resaved_4DL_normalized.nii.gz_csf_8.nii.gz  _resaved_4DL_normalized.nii.gz_csf_1.nii.gz   _resaved_4DL_normalized.nii.gz_csf_6.nii.gz   _resaved_4DL_normalized.nii.gz_csf_2.nii.gz   _resaved_4DL_normalized.nii.gz_csf_5.nii.gz  _resaved_4DL_normalized.nii.gz_csf_7.nii.gz  _resaved_4DL_normalized.nii.gz_csf_9.nii.gz  _resaved_4DL_normalized.nii.gz_csf_10.nii.gz)
-outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}" )
-done < <( tail -n +2 "${niftifile_csvfilename}" )
-################################################
-echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
-#echo "outputfiles_present::ATUL${outputfiles_present}::outputfiles_present"
-if [[  "${outputfiles_present: -1}" -eq 1 ]] ; then
-  echo " I AM THE ONE"
-fi
-if [[  "${outputfiles_present: -1}" -eq 0 ]] ; then
+  ########################################
+  outputfiles_present=0
+  while IFS=',' read -ra array; do
+    scanID=${array[2]}
+    echo sessionId::${sessionID}
+    echo scanId::${scanID}
+    resource_foldername="PREPROCESS_SEGM"
+    ### check if the file exists:
+    call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${resource_foldername} _resaved.nii.gz _resaved_4DL_normalized.nii.gz _resaved_levelset.nii.gz _resaved_4DL_seg.nii.gz _resaved_levelset_bet.nii.gz manual_splits.txt) ## _resaved_4DL_normalized.nii.gz_csf_3.nii.gz  _resaved_4DL_normalized.nii.gz_infarct.nii.gz  _resaved_4DL_normalized.nii.gz_csf_4.nii.gz  _resaved_4DL_normalized.nii.gz_csf_8.nii.gz  _resaved_4DL_normalized.nii.gz_csf_1.nii.gz   _resaved_4DL_normalized.nii.gz_csf_6.nii.gz   _resaved_4DL_normalized.nii.gz_csf_2.nii.gz   _resaved_4DL_normalized.nii.gz_csf_5.nii.gz  _resaved_4DL_normalized.nii.gz_csf_7.nii.gz  _resaved_4DL_normalized.nii.gz_csf_9.nii.gz  _resaved_4DL_normalized.nii.gz_csf_10.nii.gz)
+    outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
+  done < <(tail -n +2 "${niftifile_csvfilename}")
+  ################################################
+  echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
+  #echo "outputfiles_present::ATUL${outputfiles_present}::outputfiles_present"
+  if [[ "${outputfiles_present: -1}" -eq 1 ]]; then
+    echo " I AM THE ONE"
+  fi
+  if [[ "${outputfiles_present: -1}" -eq 0 ]]; then
 
-echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
+    echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
 
-copy_scan_data ${niftifile_csvfilename} ${working_dir}
-working_dir=/workinginput
-output_directory=/workingoutput
+    copy_scan_data ${niftifile_csvfilename} ${working_dir}
+    working_dir=/workinginput
+    output_directory=/workingoutput
 
-final_output_directory=/outputinsidedocker
-############################
-#/software/Stroke_CT_Processing/stroke_ct_processing_1.sh ${working_dir} ${output_directory}
-#/software/Stroke_CT_Processing/step4_bet.sh ${output_directory}
-#/software/Stroke_CT_Processing/stroke_ct_processing_2.sh ${output_directory} ${output_directory}
+    final_output_directory=/outputinsidedocker
+    ############################
+    #/software/Stroke_CT_Processing/stroke_ct_processing_1.sh ${working_dir} ${output_directory}
+    #/software/Stroke_CT_Processing/step4_bet.sh ${output_directory}
+    #/software/Stroke_CT_Processing/stroke_ct_processing_2.sh ${output_directory} ${output_directory}
 
-######################
-while IFS=',' read -ra array; do
-scanID=${array[2]}
-echo sessionId::${sessionID}
-echo scanId::${scanID}
-done < <( tail -n +2 "${niftifile_csvfilename}" )
-resource_dirname='PREPROCESS_SEGM'
-output_dirname=${working_dir}
+    ######################
+    while IFS=',' read -ra array; do
+      scanID=${array[2]}
+      echo sessionId::${sessionID}
+      echo scanId::${scanID}
+    done < <(tail -n +2 "${niftifile_csvfilename}")
+    resource_dirname='PREPROCESS_SEGM'
+    output_dirname=${working_dir}
 
-echo working_dir::${working_dir}
-echo output_dirname::${output_dirname}
-copy_allfiles_data   ${sessionID}  ${scanID} ${resource_dirname} ${output_dirname}
-####################
-#/bin/bash -i -c
-#/root/anaconda3/bin/conda activate tf
-/software/unet_ich_edema/ppredict.sh ${output_dirname}
+    echo working_dir::${working_dir}
+    echo output_dirname::${output_dirname}
+    copy_allfiles_data ${sessionID} ${scanID} ${resource_dirname} ${output_dirname}
+    ####################
+    #/bin/bash -i -c
+    #/root/anaconda3/bin/conda activate tf
+    /software/unet_ich_edema/ppredict.sh ${output_dirname}
 
-######################################################################################################################
-##/root/anaconda3/bin/conda deactivate
-#if [[ "$STR" == *"$SUB"* ]]; then
-#  echo "It's there."
-#fi
-for file in ${output_dirname}/*levelset*.nii.gz
-do
+    ######################################################################################################################
+    ##/root/anaconda3/bin/conda deactivate
+    #if [[ "$STR" == *"$SUB"* ]]; then
+    #  echo "It's there."
+    #fi
+    for file in ${output_dirname}/*levelset*.nii.gz; do
 
       cp ${file} ${final_output_directory}/
 
-done
+    done
 
-for file in /software/unet_ich_edema/Results_model_114/*.nii*
-do
-  if [[ "${file}" != *"ERICH"* ]]; then
-    echo "It's there."
-      new_filename=${file%.nii.gz_class1*}_class1.nii.gz
+    for file in /software/unet_ich_edema/Results_model_114/*.nii*; do
+      if [[ "${file}" != *"ERICH"* ]]; then
+        echo "It's there."
+        new_filename=${file%.nii.gz_class1*}_class1.nii.gz
         mv ${file} ${new_filename}
-      cp ${new_filename} ${final_output_directory}/
+        cp ${new_filename} ${final_output_directory}/
+      fi
+
+    done
+
+    for file in /software/unet_ich_edema/Results_model_72/*.nii*; do
+      if [[ "${file}" != *"ERICH"* ]]; then
+        echo "It's there."
+        new_filename=${file%.nii.gz_class2*}_class2.nii.gz
+        mv ${file} ${new_filename}
+        cp ${new_filename} ${final_output_directory}/
+      fi
+    done
+    ######################################################################################################################
+
+    ######################################################################################################################
+    # COPY IT TO THE SNIPR RESPECTIVE SCAN RESOURCES
+
+    snipr_output_foldername="MASKS"
+    file_suffixes=(.nii.gz .nii .txt) #sys.argv[5]
+    for file_suffix in ${file_suffixes[@]}; do
+      echo "COPYING FILES TO ${snipr_output_foldername} "
+      copyoutput_to_snipr ${sessionID} ${scanID} "${final_output_directory}" ${snipr_output_foldername} ${file_suffix}
+    done
+    ######################################################################################################################
+
+    ######################################################################################################################
+    echo " FILES NOT PRESENT I AM WORKING ON IT"
+  else
+    echo " FILES ARE PRESENT "
+  ######################################################################################################################
   fi
-
-
-done
-
-for file in /software/unet_ich_edema/Results_model_72/*.nii*
-do
-    if [[ "${file}" != *"ERICH"* ]]; then
-      echo "It's there."
-  new_filename=${file%.nii.gz_class2*}_class2.nii.gz
-  mv ${file} ${new_filename}
-  cp ${new_filename} ${final_output_directory}/
-  fi
-done
-######################################################################################################################
-
-######################################################################################################################
-# COPY IT TO THE SNIPR RESPECTIVE SCAN RESOURCES
-
-snipr_output_foldername="MASKS"
-file_suffixes=(  .nii.gz .nii .txt ) #sys.argv[5]
-for file_suffix in ${file_suffixes[@]}
-do
-    echo "COPYING FILES TO ${snipr_output_foldername} "
-    copyoutput_to_snipr  ${sessionID} ${scanID} "${final_output_directory}"  ${snipr_output_foldername}  ${file_suffix}
-done
-######################################################################################################################
-
-######################################################################################################################
-echo " FILES NOT PRESENT I AM WORKING ON IT"
-else
-  echo " FILES ARE PRESENT "
-######################################################################################################################
-fi
+  rm ${final_output_directory}/*.*
 done
 
 ################################################################################################################
@@ -278,4 +261,3 @@ done
 #    copyoutput_to_snipr  ${sessionID} ${scanID} "${final_output_directory}"  ${snipr_output_foldername}  ${file_suffix}
 #done
 #######################################################################################################################
-

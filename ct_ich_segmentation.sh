b74a9c6f907c248ac65462cdfeb5935a2ada13d3
dharlabwustl <atulkumar@wustl.edu>
2023-12-01 15:09:28 -0600

    Commit day10

--- ct_ich_segmentation.sh
+++ ct_ich_segmentation.sh
@@ -145,15 +145,15 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
     scanID=${array[2]}
     echo sessionId::${sessionID}
     echo scanId::${scanID}
-    resource_foldername="PREPROCESS_SEGM"
+    resource_foldername="MASKS" ##"PREPROCESS_SEGM"
     ### check if the file exists:
-    call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${resource_foldername} _resaved.nii.gz _resaved_4DL_normalized.nii.gz _resaved_levelset.nii.gz _resaved_4DL_seg.nii.gz _resaved_levelset_bet.nii.gz manual_splits.txt) ## _resaved_4DL_normalized.nii.gz_csf_3.nii.gz  _resaved_4DL_normalized.nii.gz_infarct.nii.gz  _resaved_4DL_normalized.nii.gz_csf_4.nii.gz  _resaved_4DL_normalized.nii.gz_csf_8.nii.gz  _resaved_4DL_normalized.nii.gz_csf_1.nii.gz   _resaved_4DL_normalized.nii.gz_csf_6.nii.gz   _resaved_4DL_normalized.nii.gz_csf_2.nii.gz   _resaved_4DL_normalized.nii.gz_csf_5.nii.gz  _resaved_4DL_normalized.nii.gz_csf_7.nii.gz  _resaved_4DL_normalized.nii.gz_csf_9.nii.gz  _resaved_4DL_normalized.nii.gz_csf_10.nii.gz)
+    call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${resource_foldername} _class1.nii.gz _class2.nii.gz ) ## _resaved_4DL_normalized.nii.gz_csf_3.nii.gz  _resaved_4DL_normalized.nii.gz_infarct.nii.gz  _resaved_4DL_normalized.nii.gz_csf_4.nii.gz  _resaved_4DL_normalized.nii.gz_csf_8.nii.gz  _resaved_4DL_normalized.nii.gz_csf_1.nii.gz   _resaved_4DL_normalized.nii.gz_csf_6.nii.gz   _resaved_4DL_normalized.nii.gz_csf_2.nii.gz   _resaved_4DL_normalized.nii.gz_csf_5.nii.gz  _resaved_4DL_normalized.nii.gz_csf_7.nii.gz  _resaved_4DL_normalized.nii.gz_csf_9.nii.gz  _resaved_4DL_normalized.nii.gz_csf_10.nii.gz)
     outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
   done < <(tail -n +2 "${niftifile_csvfilename}")
   ################################################
   echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
   #echo "outputfiles_present::ATUL${outputfiles_present}::outputfiles_present"
-  outputfiles_present=0000000000000000000000
+#  outputfiles_present=0000000000000000000000
   if [[ "${outputfiles_present: -1}" -eq 1 ]]; then
     echo " I AM THE ONE"
   fi

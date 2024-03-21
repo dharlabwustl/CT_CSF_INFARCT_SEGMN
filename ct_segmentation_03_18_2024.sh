7a3c1616d71320b25a4f2583421cae127bd0527a
dharlabwustl <atulkumar@wustl.edu>
2024-03-21 13:24:39 -0500

    Commit day10

--- ct_segmentation_03_18_2024.sh
+++ ct_segmentation_03_18_2024.sh
@@ -212,7 +212,7 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
     call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${resource_foldername} _resaved.nii.gz _resaved_4DL_normalized.nii.gz _resaved_levelset.nii.gz _resaved_4DL_seg.nii.gz _resaved_levelset_bet.nii.gz manual_splits.txt _resaved_4DL_normalized.nii.gz_csf_3.nii.gz _resaved_4DL_normalized.nii.gz_infarct.nii.gz _resaved_4DL_normalized.nii.gz_csf_4.nii.gz _resaved_4DL_normalized.nii.gz_csf_8.nii.gz _resaved_4DL_normalized.nii.gz_csf_1.nii.gz _resaved_4DL_normalized.nii.gz_csf_6.nii.gz _resaved_4DL_normalized.nii.gz_csf_2.nii.gz _resaved_4DL_normalized.nii.gz_csf_5.nii.gz _resaved_4DL_normalized.nii.gz_csf_7.nii.gz _resaved_4DL_normalized.nii.gz_csf_9.nii.gz _resaved_4DL_normalized.nii.gz_csf_10.nii.gz)
     outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
     csvfile_for_redcap='/workinginput/csvfile_for_redcap_segm.csv'
-    csvfile_scan_selection_for_redcap_call=('csvfile_preprocessing_for_redcap' ${this_csvfilename} ${csvfile_for_redcap}) # 'Name' scan_name)
+    csvfile_scan_selection_for_redcap_call=('csvfile_segmn_for_redcap' ${this_csvfilename} ${csvfile_for_redcap}) # 'Name' scan_name)
     outputfiles_present=$(python3 fillmaster_session_list.py "${csvfile_scan_selection_for_redcap_call[@]}")
 
     this_session_id=${sessionID}

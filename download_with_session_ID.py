16133a577b9ec07cca72627efe114006a0cd0633
dharlabwustl <atulkumar@wustl.edu>
2024-04-02 16:56:29 -0500

    Commit day10

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -703,7 +703,7 @@ def check_if_a_file_exist_in_snipr(URI, resource_dir,extension_to_find_list):
         for x in range(df_scan.shape[0]):
             print(df_scan.loc[x,'Name'])
             if extension_to_find in df_scan.loc[x,'Name']:
-                subprocess.call("echo " + "I FILENAME AT ::{}  >> /workingoutput/error.txt".format(df_scan.loc[x,'URI']) ,shell=True )
+                # subprocess.call("echo " + "I FILENAME AT ::{}  >> /workingoutput/error.txt".format(df_scan.loc[x,'URI']) ,shell=True )
                 num_files_present=num_files_present+1
 
     return num_files_present
@@ -719,17 +719,17 @@ def call_check_if_a_file_exist_in_snipr( args):
     extension_to_find_list=args.stuff[4:]
     file_present=check_if_a_file_exist_in_snipr(URI, resource_dir,extension_to_find_list)
     all_files_present_flag=0
+    subprocess.call("echo " + "I extension_to_find_list AT ::{}  >> /workingoutput/error.txt".format(len(extension_to_find_list)) ,shell=True )
+    subprocess.call("echo " + "I file_present AT ::{}  >> /workingoutput/error.txt".format(file_present) ,shell=True )
     if file_present < len(extension_to_find_list):
-        subprocess.call("echo " + "I extension_to_find_list AT ::{}  >> /workingoutput/error.txt".format(len(extension_to_find_list)) ,shell=True )
-        subprocess.call("echo " + "I file_present AT ::{}  >> /workingoutput/error.txt".format(file_present) ,shell=True )
         return 0
     else:
         all_files_present_flag=1
     all_files_present_flag_df=pd.DataFrame([all_files_present_flag])
     all_files_present_flag_df.columns=['all_files_present_flag']
     all_files_present_flag_df.to_csv('/workinginput/all_files_present_flag_df.csv',index=False)
-    subprocess.call("echo " + "I len(extension_to_find_list) AT ::{}  >> /workingoutput/error.txt".format(len(extension_to_find_list)) ,shell=True )
-    subprocess.call("echo " + "I file_present AT ::{}  >> /workingoutput/error.txt".format(file_present) ,shell=True )
+    # subprocess.call("echo " + "I len(extension_to_find_list) AT ::{}  >> /workingoutput/error.txt".format(len(extension_to_find_list)) ,shell=True )
+    # subprocess.call("echo " + "I file_present AT ::{}  >> /workingoutput/error.txt".format(file_present) ,shell=True )
     return 1
 def download_a_singlefile_with_URIString(url,filename,dir_to_save):
     xnatSession = XnatSession(username=XNAT_USER, password=XNAT_PASS, host=XNAT_HOST)
